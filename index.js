41e8eb11df7dfbc885b208ae671ffac710f98bfb
Guilherme Moura <gmoura.nas@gmail.com>
2018-08-29 19:15:15 -0300

    Initial commit

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ $(document).ready(() => {
   }
 
   for (name of usersName) {
-    $(".meuSeletor").append(`<option value="${name}">${name}</option>`);
+    $(".mySelector").append(`<option value="${name}">${name}</option>`);
   }
+
+  var seletor = document.getElementsByClassName("mySelector")[0];
+  seletor.addEventListener('change', function(e) {
+    let selectedUserName = e.target.value;
+    
+    // o _ vem do Loadash. O que é isso?
+    var filteredArray = _.filter(data, {name: selectedUserName});
+
+
+    for (post of filteredArray) {
+      var list = document.createElement("li");
+      const header = document.createElement("span");
+      header.textContent = "O que a " + selectedUserName + " disse:";
+      let text = document.createElement("span");
+      text.textContent = post.text;
+      list.appendChild(header);
+      list.appendChild(text);
+      postList = document.getElementsByClassName("posts-list")[0];
+      postList.appendChild(list);
+    }
+  });
 });
