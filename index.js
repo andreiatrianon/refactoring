7afcf65ba45d5665e32f270f12d8d1b87f586e44
Andreia Trianon <andreiatrianon@outlook.com>
2018-08-30 16:19:31 -0300

    index.js: criacao da function inserTasksOnList

--- index.js
+++ index.js
@@ -1,48 +1,7 @@
 // data is a variable from data.js
 $(document).ready(() => {
-
   insertNamesOnSelector();
-
-  var seletor = document.getElementsByClassName("mySelector")[0];
-  seletor.addEventListener('change', function(e) {
-    var taskList = document.getElementsByClassName("tasksList")[0];
-    taskList.innerHTML = "";
-
-    let selectedUserName = e.target.value;
-
-    // o _ é o Loadash. O que é isso?
-    var filteredArray = _.filter(data, {name: selectedUserName});
-
-
-
-
-    for (task of filteredArray) {
-      var list = document.createElement("li");
-      let text = document.createElement("span");
-      list.dataset.id=task.id;
-      text.textContent = task.title;
-      list.appendChild(text);
-      taskList = document.getElementsByClassName("tasksList")[0];
-      list.className = "tasks-list-item";
-      taskList.appendChild(list);
-    }
-
-
-    //risca o texto
-    var allItensFromList = document.getElementsByTagName("li");
-    for (listItem of allItensFromList) {
-      let itemId = listItem.getAttribute('data-id');
-      for (task of filteredArray) {
-        if (parseInt(itemId) == task.id) {
-          if (task.completed) {
-            listItem.style = "text-decoration: line-through;"
-          }
-        }
-      }
-
-
-    }
-  });
+  $('.mySelector').change(inserTasksOnList);
 });
 
 function insertNamesOnSelector() {
@@ -62,4 +21,36 @@ function createOptionsOnSelector(userNamesFromDB) {
   userNamesFromDB.map((name) => {
     $(".mySelector").append(`<option value="${name}">${name}</option>`);
   });
+}
+
+function inserTasksOnList() {
+  $(".tasksList").html = "";
+  let selectedUserName = $(this).find(":selected").text();
+
+  // o _ é o Loadash
+  var filteredArray = _.filter(data, {name: selectedUserName});
+
+  for (task of filteredArray) {
+    var list = document.createElement("li");
+    let text = document.createElement("span");
+    list.dataset.id=task.id;
+    text.textContent = task.title;
+    list.appendChild(text);
+    taskList = document.getElementsByClassName("tasksList")[0];
+    list.className = "tasks-list-item";
+    taskList.appendChild(list);
+  }
+
+  //risca o texto
+  var allItensFromList = document.getElementsByTagName("li");
+  for (listItem of allItensFromList) {
+    let itemId = listItem.getAttribute('data-id');
+    for (task of filteredArray) {
+      if (parseInt(itemId) == task.id) {
+        if (task.completed) {
+          listItem.style = "text-decoration: line-through;"
+        }
+      }
+    }
+  }
 }
